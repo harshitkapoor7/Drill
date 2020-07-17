c78501eedf5e3fc12f78fc64f3288bb8c11446e7
arushacharyamnnit <arushacharya459@gmail.com>
2020-07-17 23:11:41 +0530

    news before adapter

--- app/src/main/java/com/example/alpha/api/ApiInterface.java
+++ app/src/main/java/com/example/alpha/api/ApiInterface.java
@@ -8,5 +8,6 @@ import retrofit2.http.Query;
 
 public interface ApiInterface {
     @GET("top-headlines")
+
     Call<News> getNews(@Query("country") String country,@Query("apiKey") String apiKey);
 }
