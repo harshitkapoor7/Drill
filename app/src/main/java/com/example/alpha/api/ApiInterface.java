97150e6f50a87867ceac76f2ac511c706f23253c
arushacharyamnnit <arushacharya459@gmail.com>
2020-07-18 08:19:29 +0530

    news before adapter

--- app/src/main/java/com/example/alpha/api/ApiInterface.java
+++ app/src/main/java/com/example/alpha/api/ApiInterface.java
@@ -5,6 +5,7 @@ import com.example.alpha.models.News;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
+
 public interface ApiInterface {
     @GET("top-headlines")
 
